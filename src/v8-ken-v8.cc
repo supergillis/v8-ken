f787450b07ef933a90662727418e5cb2c5c89eb6
Gillis Van Ginderachter <supergillis@gmail.com>
2013-03-20 21:10:10 +0100

    Added stringify and parse when sending and receiving messages.

--- src/v8-ken-v8.cc
+++ src/v8-ken-v8.cc
@@ -61,31 +61,40 @@ namespace v8 {
       String::Utf8Value message(tryCatch->Exception());
       fprintf(stderr, "Unhandled exception: %s\n", ToCString(message));
 
-      // Crash the application on non-native (user-thrown) errors
-      Handle<Value> exception = tryCatch->Exception();
-      if (!exception.IsEmpty() && !exception->IsNativeError())
-        abort();
-
       return true;
     }
 
-    bool HandleReceive(const char* sender, const char* string, int32_t length) {
+    bool HandleReceive(const char* sender, const char* message, int32_t length) {
       HandleScope handle_scope;
 
       Handle<Object> global = Context::GetCurrent()->Global();
       Handle<Value> value = global->Get(String::New("receive"));
 
-      if (value->IsFunction()) {
-        Handle<Function> ken_receive = Handle<Function>::Cast(value);
-        Handle<Value> args[2];
-        args[0] = String::New(sender);
-        args[1] = String::New(string, length);
-
-        ken_receive->Call(global, 2, args);
-        return true;
+      if (!value->IsFunction()) {
+        return false;
       }
 
-      return false;
+      Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
+      Handle<Function> JSON_parse = Handle<Function>::Cast(JSON->Get(String::New("parse")));
+      Handle<Value> JSON_parse_args[1];
+      JSON_parse_args[0] = String::New(message, length);
+
+      // Try to parse the JSON strinĝ
+      TryCatch tryCatch;
+      Handle<Value> result = JSON_parse->Call(global, 1, JSON_parse_args);
+
+      if (tryCatch.HasCaught()) {
+        HandleException(&tryCatch);
+        return false;
+      }
+
+      Handle<Function> ken_receive = Handle<Function>::Cast(value);
+      Handle<Value> ken_receive_args[2];
+      ken_receive_args[0] = String::New(sender);
+      ken_receive_args[1] = result;
+
+      ken_receive->Call(global, 2, ken_receive_args);
+      return true;
     }
 
     Handle<Value> Print(const Arguments& args) {
@@ -101,14 +110,30 @@ namespace v8 {
     }
 
     Handle<Value> Send(const Arguments& args) {
-      String::Utf8Value kenid_string(args[0]);
-      String::Utf8Value msg_string(args[1]);
+      HandleScope handle_scope;
 
+      Handle<Object> global = Context::GetCurrent()->Global();
+      Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
+      Handle<Function> JSON_stringify = Handle<Function>::Cast(JSON->Get(String::New("stringify")));
+      Handle<Value> JSON_stringify_args[1];
+      JSON_stringify_args[0] = args[1];
+
+      // Try to stringify the argument
+      TryCatch tryCatch;
+      Handle<Value> result = JSON_stringify->Call(global, 1, JSON_stringify_args);
+
+      if (tryCatch.HasCaught()) {
+        return ThrowException(tryCatch.Exception());
+      }
+
+      String::Utf8Value kenid_string(args[0]);
       const char* kenid = ToCString(kenid_string);
-      const char* msg = ToCString(msg_string);
+
+      String::Utf8Value stringified_string(result);
+      const char* stringified = ToCString(stringified_string);
 
       // Call the primitive ken_send
-      seqno_t seqno = ken_send(ken_id_from_string(kenid), msg, strlen(msg));
+      seqno_t seqno = ken_send(ken_id_from_string(kenid), stringified, strlen(stringified));
 
       return Integer::New(seqno);
     }
