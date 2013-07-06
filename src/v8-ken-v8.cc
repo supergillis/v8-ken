7988bec38dea63ac52ab85722267d1d78ce5e9b7
Gillis Van Ginderachter <supergillis@gmail.com>
2013-07-06 12:58:42 +0200

    Fixed wrong usage of handle scopes.

--- src/v8-ken-v8.cc
+++ src/v8-ken-v8.cc
@@ -36,6 +36,7 @@ namespace v8 {
 
     Handle<Value> JSONStringify(Handle<Value> object) {
       HandleScope handle_scope;
+      Handle<Value> result;
 
       Handle<Object> global = Context::GetCurrent()->Global();
       Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
@@ -44,11 +45,13 @@ namespace v8 {
       Handle<Value> args[1];
       args[0] = object;
 
-      return JSON_stringify->Call(global, 1, args);
+      result = JSON_stringify->Call(global, 1, args);
+      return handle_scope.Close(result);
     }
 
     Handle<Value> JSONParse(Handle<String> json) {
       HandleScope handle_scope;
+      Handle<Value> result;
 
       Handle<Object> global = Context::GetCurrent()->Global();
       Handle<Object> JSON = global->Get(String::New("JSON"))->ToObject();
@@ -57,30 +60,32 @@ namespace v8 {
       Handle<Value> args[1];
       args[0] = json;
 
-      return JSON_parse->Call(global, 1, args);
+      result = JSON_parse->Call(global, 1, args);
+      return handle_scope.Close(result);
     }
 
     Handle<Value> Eval(const char* file, const char* source, int32_t length) {
       HandleScope handle_scope;
+      Handle<Value> result;
+
       Handle<String> file_string = String::New(file);
       Handle<String> source_string = String::New(source, length);
-      return Eval(file_string, source_string);
+      result = Eval(file_string, source_string);
+      return handle_scope.Close(result);
     }
 
     Handle<Value> Eval(Handle<String> file, Handle<String> source) {
       HandleScope handle_scope;
+      Handle<Value> result;
+
       Handle<Script> script = Script::Compile(source, file);
-
       if (script.IsEmpty()) {
-        return Undefined();
+        result = Undefined();
+      } else {
+        result = script->Run();
       }
 
-      Handle<Value> result = script->Run();
-      if (result.IsEmpty()) {
-        return Undefined();
-      }
-
-      return result;
+      return handle_scope.Close(result);
     }
 
     void ReportException(v8::TryCatch* try_catch) {
@@ -128,40 +133,55 @@ namespace v8 {
 
     Handle<Value> HandleReceive(Handle<String> sender, Handle<String> message) {
       HandleScope handle_scope;
+      Handle<Value> result;
 
       Handle<Object> global = Context::GetCurrent()->Global();
       Handle<Value> function = global->Get(String::New("receive"));
 
       if (!function->IsFunction()) {
-        return Undefined();
+        result = Undefined();
+      } else {
+        Handle<Function> ken_receive = Handle<Function>::Cast(function);
+        Handle<Value> ken_receive_args[2];
+        ken_receive_args[0] = sender;
+        ken_receive_args[1] = JSONStringify(message);
+
+        result = ken_receive->Call(global, 2, ken_receive_args);
       }
 
-      Handle<Function> ken_receive = Handle<Function>::Cast(function);
-      Handle<Value> ken_receive_args[2];
-      ken_receive_args[0] = sender;
-      ken_receive_args[1] = JSONStringify(message);
-
-      return ken_receive->Call(global, 2, ken_receive_args);
+      return handle_scope.Close(result);
     }
 
+    /**
+     *
+     */
     Handle<Value> HandleHttpRequest(Handle<Object> request, Handle<Object> response) {
       HandleScope handle_scope;
+      Handle<Value> result;
 
       Handle<Object> global = Context::GetCurrent()->Global();
       Handle<Value> function = global->Get(String::New("handleRequest"));
 
       if (!function->IsFunction()) {
-        return Undefined();
+        result = Undefined();
+      } else {
+        Handle<Function> handle_request = Handle<Function>::Cast(function);
+        Handle<Value> handle_request_args[2];
+        handle_request_args[0] = request;
+        handle_request_args[1] = response;
+
+        result = handle_request->Call(global, 2, handle_request_args);
       }
 
-      Handle<Function> handle_request = Handle<Function>::Cast(function);
-      Handle<Value> handle_request_args[2];
-      handle_request_args[0] = request;
-      handle_request_args[1] = response;
-
-      return handle_request->Call(global, 2, handle_request_args);
+      return handle_scope.Close(result);
     }
 
+    /**
+     * We do not enter a handle scope here. This function must be called using
+     * code that is already in a handle scope.
+     *
+     * @param name
+     */
     Handle<String> ReadFile(const char* name) {
       FILE* file = fopen(name, "rb");
       if (file == NULL) {
@@ -187,6 +207,7 @@ namespace v8 {
 
     Handle<Value> Print(const Arguments& args) {
       HandleScope handle_scope;
+      Handle<Value> result;
 
       for (int index = 0; index < args.Length(); index++) {
         String::Utf8Value string(args[index]);
@@ -195,106 +216,133 @@ namespace v8 {
 
       print("\n");
 
-      return Undefined();
+      result = Undefined();
+      return handle_scope.Close(result);
     }
 
     Handle<Value> Send(const Arguments& args) {
       HandleScope handle_scope;
+      Handle<Value> result;
+      Handle<Value> kenid_val;
+      Handle<Value> message_val;
 
-      Handle<Value> kenid_val = args[0];
+      kenid_val = args[0];
       if (!kenid_val->IsString()) {
-        return ThrowException(String::New("Expected a string as first argument!"));
+        result = ThrowException(String::New("Expected a string as first argument!"));
+      } else {
+        message_val = JSONStringify(args[1]);
+        if (!message_val->IsString()) {
+          result = ThrowException(String::New("Expected an object that can be stringified as second argument!"));
+        } else {
+          String::Utf8Value kenid(kenid_val->ToString());
+          String::Utf8Value message(message_val->ToString());
+
+          // Call the primitive ken_send
+          seqno_t seqno = ken_send(ken_id_from_string(*kenid), *message, strlen(*message));
+
+          result = Integer::New(seqno);
+        }
       }
 
-      Handle<Value> message_val = JSONStringify(args[1]);
-      if (!message_val->IsString()) {
-        return ThrowException(String::New("Expected an object that can be stringified as second argument!"));
-      }
-
-      String::Utf8Value kenid(kenid_val->ToString());
-      String::Utf8Value message(message_val->ToString());
-
-      // Call the primitive ken_send
-      seqno_t seqno = ken_send(ken_id_from_string(*kenid), *message, strlen(*message));
-
-      return Integer::New(seqno);
+      return handle_scope.Close(result);
     }
 
     Handle<Value> Read(const Arguments& args) {
+      HandleScope handle_scope;
+      Handle<Value> result;
+      Handle<String> file;
+      Handle<String> source;
+
       if (args.Length() != 1 || !args[0]->IsString()) {
-        return ThrowException(String::New("Read expects a string as first parameter!"));
+        result = ThrowException(String::New("Read expects a string as first parameter!"));
+      } else {
+        file = args[0]->ToString();
+        source = ReadFile(*String::Utf8Value(file));
+        if (source.IsEmpty()) {
+          result = ThrowException(String::Concat(String::New("Error reading file: "), file));
+        } else {
+          result = source;
+        }
       }
 
-      Handle<String> file = args[0]->ToString();
-      Handle<String> source = ReadFile(*String::Utf8Value(file));
-      if (source.IsEmpty()) {
-        return ThrowException(String::Concat(String::New("Error reading file: "), file));
-      }
-
-      return source;
+      return handle_scope.Close(result);
     }
 
     Handle<Value> Load(const Arguments& args) {
+      HandleScope handle_scope;
+      Handle<Value> result;
+
       if (args.Length() != 1 || !args[0]->IsString()) {
-        return ThrowException(String::New("Load expects a string as first parameter!"));
+        result = ThrowException(String::New("Load expects a string as first parameter!"));
+      } else {
+        result = Read(args);
+
+        // Stringify if it's a string
+        if (result->IsString()) {
+          result = Eval(args[0]->ToString(), result->ToString());
+        }
       }
 
-      Handle<Value> value = Read(args);
-      if (!value->IsString()) {
-        return value;
-      }
-
-      return Eval(args[0]->ToString(), value->ToString());
+      return handle_scope.Close(result);
     }
 
     Handle<Value> Eval(const Arguments& args) {
+      HandleScope handle_scope;
+      Handle<Value> result;
+
       if (args.Length() != 1 || !args[0]->IsString()) {
-        return ThrowException(String::New("Eval expects a string as first parameter!"));
+        result = ThrowException(String::New("Eval expects a string as first parameter!"));
+      } else {
+        result = Eval(String::New("eval"), args[0]->ToString());
       }
 
-      return Eval(String::New("eval"), args[0]->ToString());
+      return handle_scope.Close(result);
     }
 
     Handle<Value> HrTime(const Arguments& args) {
+      HandleScope handle_scope;
+      Handle<Value> result;
+      Handle<Array> array;
+      Handle<Array> start;
+
       struct timespec ts;
       clock_gettime(CLOCK_MONOTONIC, &ts);
 
       if (args.Length() == 0) {
-        Handle<Array> result = Array::New(2);
-        result->Set(0, Integer::New(ts.tv_sec));
-        result->Set(1, Integer::New(ts.tv_nsec));
-        return result;
-      }
+        array = Array::New(2);
+        array->Set(0, Integer::New(ts.tv_sec));
+        array->Set(1, Integer::New(ts.tv_nsec));
+        result = array;
+      } else if (args.Length() != 1 || !args[0]->IsArray()) {
+        result = ThrowException(String::New("HrTime expects no parameters, or time as first argument!"));
+      } else {
+        start = Handle<Array>::Cast(args[0]);
+        if (!start->Get(0)->IsNumber() || !start->Get(1)->IsNumber()) {
+          result = ThrowException(String::New("HrTime expects no parameters, or time as first argument!"));
+        } else {
+          int64_t elapsed_sec = ts.tv_sec - start->Get(0)->ToInteger()->IntegerValue();
+          int64_t elapsed_nsec = ts.tv_nsec - start->Get(1)->ToInteger()->IntegerValue();
 
-      if (args.Length() != 1 || !args[0]->IsArray()) {
-        return ThrowException(String::New("HrTime expects no parameters, or time as first argument!"));
-      }
+          if (elapsed_sec > 0) {
+            if (elapsed_nsec < 0) {
+              elapsed_nsec += 1e9;
+              elapsed_sec -= 1;
+            }
+          } else if (elapsed_sec < 0) {
+            if (elapsed_nsec > 0) {
+              elapsed_nsec -= 1e9;
+              elapsed_sec += 1;
+            }
+          }
 
-      Handle<Array> start = Handle<Array>::Cast(args[0]);
-      if (!start->Get(0)->IsNumber() || !start->Get(1)->IsNumber()) {
-        return ThrowException(String::New("HrTime expects no parameters, or time as first argument!"));
-      }
-
-      int64_t elapsed_sec = ts.tv_sec - start->Get(0)->ToInteger()->IntegerValue();
-      int64_t elapsed_nsec = ts.tv_nsec - start->Get(1)->ToInteger()->IntegerValue();
-
-      if (elapsed_sec > 0) {
-        if (elapsed_nsec < 0) {
-          elapsed_nsec += 1e9;
-          elapsed_sec -= 1;
+          array = Array::New(2);
+          array->Set(0, Integer::New(elapsed_sec));
+          array->Set(1, Integer::New(elapsed_nsec));
+          result = array;
         }
       }
-      else if (elapsed_sec < 0) {
-        if (elapsed_nsec > 0) {
-          elapsed_nsec -= 1e9;
-          elapsed_sec += 1;
-        }
-      }
 
-      Handle<Array> result = Array::New(2);
-      result->Set(0, Integer::New(elapsed_sec));
-      result->Set(1, Integer::New(elapsed_nsec));
-      return result;
+      return handle_scope.Close(result);
     }
   }
 }
